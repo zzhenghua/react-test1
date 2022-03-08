88ae5d065d4b74c25687444bb62de4b851c77f7b
zengzhenghua <1257049723>
2022-03-09 00:08:50 +0800

    2

--- src/components/hello.js
+++ src/components/hello.js
@@ -3,13 +3,13 @@
  * @Author: Zeng
  * @Date: 2022-02-18 01:05:58
  * @LastEditors: Zeng
- * @LastEditTime: 2022-03-08 23:52:32
+ * @LastEditTime: 2022-03-09 00:08:06
  */
 import React from "react";
 
 class Hello extends React.Component {
   render() {
-    return (<h1 style={{color: 'red'}}>hello react1</h1>)
+    return (<h1 style={{color: 'red'}}>hello react2</h1>)
   }
 }
 
