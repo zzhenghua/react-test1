1160172f6a3e9d08ac75695aabb60460dcc31f92
zengzhenghua <1257049723>
2022-03-09 00:27:31 +0800

    Revert "3"
    This reverts commit 2e8611289ad42b5dbb87afc9aef573098203d414.

--- src/components/hello.js
+++ src/components/hello.js
@@ -3,7 +3,11 @@
  * @Author: Zeng
  * @Date: 2022-02-18 01:05:58
  * @LastEditors: Zeng
- * @LastEditTime: 2022-03-09 00:23:56
+<<<<<<< HEAD
+ * @LastEditTime: 2022-03-09 00:27:03
+=======
+ * @LastEditTime: 2022-03-09 00:08:06
+>>>>>>> parent of 2e86112 (3)
  */
 import React from "react";
 
