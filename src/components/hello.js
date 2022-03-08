fd2d8c69c9b1029bb54098a46d992e479098db3e
zengzhenghua <1257049723>
2022-03-09 00:25:26 +0800

    新内容

--- src/components/hello.js
+++ src/components/hello.js
@@ -3,13 +3,18 @@
  * @Author: Zeng
  * @Date: 2022-02-18 01:05:58
  * @LastEditors: Zeng
- * @LastEditTime: 2022-03-09 00:12:52
+ * @LastEditTime: 2022-03-09 00:23:56
  */
 import React from "react";
 
 class Hello extends React.Component {
-  render() {
-    return (<h1 style={{color: 'red'}}>hello react4</h1>)
+  render () {
+    return (
+      <div>
+        <h1 style={{ color: 'red' }}>hello react4</h1>
+        这里是新的内容
+      </div>
+    )
   }
 }
 
